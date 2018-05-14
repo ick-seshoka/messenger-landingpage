e77edaa50541c811013fe51363924753f229f9ea
Isaack Seshoka <ick.seshoka@gmail.com>
2018-05-14 07:39:45 +0200

    Commit Header / Title / Subject Download section layout & reviews
    Commit message body
    Initialised user reviews scrolling and completed download section layout for tablet and desktop

--- js/main.js
+++ js/main.js
@@ -1,7 +1,5 @@
-
 $(document).ready(function() {
     // click event for menu button
-
     $('#menu-trigger').click(function(e) {
         e.preventDefault();
         var logo = $('#logo');
@@ -42,5 +40,55 @@ $(document).ready(function() {
             // enable body scroll
             $('html,body').css('overflow', 'auto');
         }
+    });
+
+    var userReviewsContainer = $('#user-reviews');
+    var userReviewWidth = $('.active-review').width();
+    var leftScrollBtn = $('#left-scroller');
+    var rightScrollBtn = $('#right-scroller');
+
+    // User reviews right scroll
+    $('#right-scroller').click(function(e) {
+        e.preventDefault();
+
+        userReviewsContainer.animate(
+            {
+                scrollLeft: userReviewsContainer.scrollLeft() + userReviewWidth
+             }
+            , 'slow', function() {
+                if (userReviewsContainer.scrollLeft() >= 0) {
+                    leftScrollBtn.removeClass('inactive');
+                    leftScrollBtn.addClass('active');
+                }
+
+                if (userReviewsContainer.scrollLeft() - userReviewWidth >= userReviewsContainer.width()) {
+                    rightScrollBtn.removeClass('active');
+                    rightScrollBtn.addClass('inactive');
+                }
+            }
+        );
+
+    });
+
+    // User reviews left scroll
+    $('#left-scroller').click(function(e) {
+        e.preventDefault();
+
+        userReviewsContainer.animate(
+            {
+                scrollLeft: userReviewsContainer.scrollLeft() - userReviewWidth
+             }
+            , 'slow', function() {
+                if (userReviewsContainer.scrollLeft() - userReviewWidth <= 0) {
+                    leftScrollBtn.removeClass('active');
+                    leftScrollBtn.addClass('inactive');
+                }
+
+                if (userReviewsContainer.scrollLeft() - userReviewWidth <= userReviewsContainer.width()) {
+                    rightScrollBtn.removeClass('inactive');
+                    rightScrollBtn.addClass('active');
+                }
+            }
+        );
     });
 });
